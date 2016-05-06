e88c98d949352982e37acf515706d7c4e201c584
Stuart Jones <Me@Stuart-Jones.com>
2016-05-06 18:18:23 -0500

    Reverting non-working code.

--- steambot.js
+++ steambot.js
@@ -11,27 +11,56 @@ module.exports = function (req, res, next) {
 
   res.status(200).send(immediateResponse);
 
-  botPayLoad = steam.find(req.body.text).then(function() {
-      // write response message and add to payload
-      botPayload.username = 'Gabe';
-      botPayload.channel = req.body.channel_id;
-      botPayload.response_type = 'in_channel';
-      botPayload.icon_emoji = ':video_game:';
-      botPayload.response_url = req.body.response_url;
-      console.log(botPayLoad);
-      // send game info
-      send(botPayload, function (error, status, body) {
-        if (error) {
-          return next(error);
+  // write response message and add to payload
+  botPayload.text = "Hey " + userName + "! Here's what I could find about <http://store.steampowered.com/app/413150|Stardew Valley> on Steam.";
+  botPayload.unfurl_links = false;
+  botPayload.attachments = [
+      {
+          "fallback": "Here's what I could find about Stardew Valley on Steam. It's $14.99 and can be found here: http://store.steampowered.com/app/413150",
+          "color": "#818182",
+          "fields": [
+              {
+                  "title": "Price",
+                  "value": "$14.99 (0% Off)",
+                  "short": true
+              },
+              {
+                  "title": "Release Date",
+                  "value": "Feb 26, 2016",
+                  "short": true
+              },
+              {
+                  "title": "Genres",
+                  "value": "Indie, RPG, Simulation",
+                  "short": true
+              },
+              {
+                  "title": "Platforms",
+                  "value": "PC, Mac",
+                  "short": true
+              }
+          ],
+          "image_url": "https://steamcdn-a.akamaihd.net/steam/apps/256660296/movie.293x165.jpg"
+      }
+  ];
+  botPayload.username = 'Gabe';
+  botPayload.channel = req.body.channel_id;
+  botPayload.response_type = 'in_channel';
+  botPayload.icon_emoji = ':video_game:';
+  botPayload.response_url = req.body.response_url;
 
-        } else if (status !== 200) {
-          // inform user that our Incoming WebHook failed
-          return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+  // send game info
+  send(botPayload, function (error, status, body) {
+    if (error) {
+      return next(error);
 
-        } else {
-          return res.status(200).end();
-        }
-      });
+    } else if (status !== 200) {
+      // inform user that our Incoming WebHook failed
+      return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+
+    } else {
+      return res.status(200).end();
+    }
   });
 }
 
