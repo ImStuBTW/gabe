d7e59c098ee3492f08ca3a15bdc893b2ca065a8a
Stuart Jones <Me@Stuart-Jones.com>
2016-05-06 17:35:10 -0500

    Express Routes For Bot

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
 var express = require('express');
-var slack = require('slack-express');
 var bodyParser = require('body-parser');
 var steam = require('./steam');
+var steambot = require('./steambot');
 
 var app = express();
-var port = 3003;
-
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!') });
-
-// basic error handler
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(400).send(err.message);
-});
-
-// hellobot
-app.post('/gabe', function(req, res, error) {
-    steam.find(req.body.text).then(function(message) {
-        message.attachments[0].fields.forEach(function(thing) {
-            console.log(thing);
-        });
-    });
-});
-
-app.listen(port, function () {
-  console.log('Slack bot listening on port ' + port);
-});
+var port = process.env.PORT || 3000;
 
 // Configure CronJob to run at midnight and noon.
 var CronJob = require('cron').CronJob;
@@ -44,3 +19,22 @@ var job = new CronJob('0 0,12 * * *', function() {
 
 // Update Database On Launch
 steam.populate();
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world, it\'s Gabe.') });
+
+// basic error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+// steambot
+app.post('/steam', steambot);
+
+app.listen(port, function () {
+  console.log('Gabe listening on port ' + port);
+});
---
+++ steambot.js
@@ -0,0 +1,78 @@
+var request = require('request');
+var steam = require('./steam');
+
+module.exports = function (req, res, next) {
+  var botPayload = {};
+
+  /*if (!req.body.text) {
+      return res.status(200).send('Please Input A Game Title');
+  }*/
+
+  // write response message and add to payload
+  botPayload.text = "Here's what I could find about <http://store.steampowered.com/app/413150|Stardew Valley> on Steam.";
+  botPayload.unfurl_links = false;
+  botPayload.attachments = [
+      {
+          "fallback": "Here's what I could find about Stardew Valley on Steam. It's $14.99 and can be found here: http://store.steampowered.com/app/413150",
+          "color": "#818182",
+          "fields": [
+              {
+                  "title": "Price",
+                  "value": "$14.99 (0% Off)",
+                  "short": true
+              },
+              {
+                  "title": "Release Date",
+                  "value": "Feb 26, 2016",
+                  "short": true
+              },
+              {
+                  "title": "Genres",
+                  "value": "Indie, RPG, Simulation",
+                  "short": true
+              },
+              {
+                  "title": "Platforms",
+                  "value": "PC, Mac",
+                  "short": true
+              }
+          ],
+          "image_url": "https://steamcdn-a.akamaihd.net/steam/apps/256660296/movie.293x165.jpg"
+      }
+  ];
+  botPayload.username = 'Gabe';
+  botPayload.channel = req.body.channel_id;
+  botPayload.icon_emoji = ':video_game:';
+
+  // send game info
+  send(botPayload, function (error, status, body) {
+    if (error) {
+      return next(error);
+
+    } else if (status !== 200) {
+      // inform user that our Incoming WebHook failed
+      return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+
+    } else {
+      return res.status(200).end();
+    }
+  });
+}
+
+
+function send (payload, callback) {
+  var path = process.env.INCOMING_WEBHOOK_PATH;
+  var uri = 'https://hooks.slack.com/services' + path;
+
+  request({
+    uri: uri,
+    method: 'POST',
+    body: JSON.stringify(payload)
+  }, function (error, response, body) {
+    if (error) {
+      return callback(error);
+    }
+
+    callback(null, response.statusCode, body);
+  });
+}
